4c9042798190b535a83719c481cb238bd48f5801
vymik <43873650+vymik@users.noreply.github.com>
2018-10-19 14:14:54 +0300

    add carousel sction to WP

--- partials/carousel.php
+++ partials/carousel.php
@@ -2,93 +2,34 @@
 	<div class="section-centered">
 		<div class="heading">
 			<h2>BRIDEMAIDS <span>and</span> GROOMSMEN'S</h2>
-			<p><span>Best Man & Maid of Honor</span></p>
+			<p><span><?php the_field('carousel_heading_description'); ?></span></p>
 		</div>
 		<div class="owl-carousel owl-theme">
 			<?php 
-			
+			if( have_rows('brides_and_grooms_repeater') ):
+			 	// loop through the rows of data
+			    while ( have_rows('brides_and_grooms_repeater') ) : the_row();
+			    	?>
+			    	<div class="carousel-column">
+						<div class="about-pic">
+							<?php 
+							$image = get_field('carousel_picture_frame');
+							$image2 = get_sub_field('picture');
+							?>
+							<img class="pic-frame" src="<?php echo $image['sizes']['medium'] ?>" alt="<?php echo $image['name']; ?>">
+							<div class="inner-round-border">
+								<img class="round-pic" src="<?php echo $image2['sizes']['medium'] ?>" alt="<?php echo $image2['name']; ?>">
+							</div>
+						</div>
+						<h3><?php the_sub_field('name'); ?></h3>
+						<p><?php the_sub_field('description'); ?></p>
+					</div>
+			    	<?php
+			        // display a sub field value
+			        // the_sub_field('sub_field_name');
+			    endwhile;
+			endif;
 			?>
-			<div class="carousel-column">
-				<div class="about-pic">
-					<img class="pic-frame" src="assets/images/about_pic_frame.png" alt="carousel_pic_frame">
-					<div class="inner-round-border">
-						<img class="round-pic" src="assets/images/bridemaids1.jpg" alt="laura">
-					</div>
-				</div>
-				<h3>laura stephanson</h3>
-				<p>Maid of Honor</p>
-			</div>
-			<div class="carousel-column">
-				<div class="about-pic">
-					<img class="pic-frame" src="assets/images/about_pic_frame.png" alt="carousel_pic_frame">
-					<div class="inner-round-border">
-						<img class="round-pic" src="assets/images/bridemaids2.jpg" alt="laura">
-					</div>
-				</div>
-				<h3>lindsey remy</h3>
-				<p>Bride Best Friend</p>
-			</div>
-			<div class="carousel-column">
-				<div class="about-pic">
-					<img class="pic-frame" src="assets/images/about_pic_frame.png"
-					 alt="carousel_pic_frame">
-					<div class="inner-round-border">
-						<img class="round-pic" src="assets/images/bridemaids3.jpg" alt="laura">
-					</div>
-				</div>
-				<h3>elizabeth lyn</h3>
-				<p>Bride Sister</p>
-			</div>
-			<div class="carousel-column">
-				<div class="about-pic">
-					<img class="pic-frame" src="assets/images/about_pic_frame.png" alt="carousel_pic_frame">
-					<div class="inner-round-border">
-						<img class="round-pic" src="assets/images/bridemaids4.jpg" alt="laura">
-					</div>
-				</div>
-				<h3>maya eliot</h3>
-				<p>Bride Friend</p>
-			</div>
-			<div class="carousel-column">
-				<div class="about-pic">
-					<img class="pic-frame" src="assets/images/about_pic_frame.png" alt="carousel_pic_frame">
-					<div class="inner-round-border">
-						<img class="round-pic" src="assets/images/bridemaids5.jpg" alt="laura">
-					</div>
-				</div>
-				<h3>elizabeth lyn</h3>
-				<p>Bride Friend</p>
-			</div>
-			<div class="carousel-column">
-				<div class="about-pic">
-					<img class="pic-frame" src="assets/images/about_pic_frame.png" alt="carousel_pic_frame">
-					<div class="inner-round-border">
-						<img class="round-pic" src="assets/images/bridemaids6.jpg" alt="laura">
-					</div>
-				</div>
-				<h3>maya eliot</h3>
-				<p>Best Friend</p>
-			</div>
-			<div class="carousel-column">
-				<div class="about-pic">
-					<img class="pic-frame" src="assets/images/about_pic_frame.png" alt="carousel_pic_frame">
-					<div class="inner-round-border">
-						<img class="round-pic" src="assets/images/groomsmen1.jpg" alt="laura">
-					</div>
-				</div>
-				<h3>steve kay</h3>
-				<p>Best Man</p>
-			</div>
-			<div class="carousel-column">
-				<div class="about-pic">
-					<img class="pic-frame" src="assets/images/about_pic_frame.png" alt="carousel_pic_frame">
-					<div class="inner-round-border">
-						<img class="round-pic" src="assets/images/groomsmen2.jpg" alt="laura">
-					</div>
-				</div>
-				<h3>john doe</h3>
-				<p>Groom`s Brother</p>
-			</div>	
 		</div>
 	</div>
 </section>
