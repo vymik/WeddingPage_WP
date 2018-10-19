0aba9136c0d22037a22dfc3b6dd7c1a6b8079276
vymik <43873650+vymik@users.noreply.github.com>
2018-10-19 16:44:26 +0300

    fixed location WP

--- partials/location.php
+++ partials/location.php
@@ -2,26 +2,39 @@
 	<div class="section-centered">
 		<div class="heading">
 			<h2>Where <span>the love</span> begins</h2>
-			<p><span>Love is composed of a single soul inhabiting two bodies</span></p>
+			<p><span><?php the_field('location_description'); ?></span></p>
 		</div>
 		<div class="location-icon">
-			<i class="fas fa-map-marker-alt"></i>
+			<?php the_field('location_icon'); ?>
 		</div>
 		<div class="maps-container">
-			<div class="map-column">
-				<h3>Church</h3>
-				<img src="assets/images/icons/church.png" alt="church">
-				<div id="map" class="map">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6620.323043926803!2d24.053046649018796!3d54.08016885718145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e09c0d0ebbae69%3A0x75af3564f37ddc37!2zTGnFoWtpYXZvcyDFoHbEjS4gVHJlanlixJdzIGJhxb5uecSNaWE!5e0!3m2!1slt!2slt!4v1539259007036" width="100%" height="300px" frameborder="0" style="border:5px solid white" allowfullscreen></iframe>
-				</div>
-			</div>
-			<div class="map-column">
-				<h3>Restaurant</h3>
-				<img src="assets/images/icons/glasses.png" alt="glasses">
-				<div id="restaurant-map" class="map">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18748.801376304702!2d23.95315628916751!3d54.02761850287816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e09b94da4b9a6f%3A0xc5a894aef5471d1d!2sAero+gallery!5e0!3m2!1slt!2slt!4v1539278303642" width="100%" height="300" frameborder="0" style="border:5px solid white" allowfullscreen></iframe>
-				</div>
-			</div>
+			<?php 
+			if( have_rows('location_maps_repeater') ):
+			 	// loop through the rows of data
+			    while ( have_rows('location_maps_repeater') ) : the_row();
+			        ?>
+			        <div class="map-column">
+						<h3><?php the_sub_field('map_title'); ?></h3>
+						<?php			
+						if(get_sub_field('map_icon_type')):
+							// jei image
+							$image = get_sub_field('icon_type_image');	
+							?>
+							<img src="<?php echo $image['sizes']['icon'] ?>" alt="<?php $image['name']; ?>">
+							<?php 
+						else:
+							// jei fontawesome icon
+							the_sub_field('icon_type_fontawesome');
+						endif; 
+						?>
+						<div id="map" class="map">
+							<iframe src="<?php the_sub_field('place_in_map'); ?>" width="100%" height="300px" frameborder="0" style="border:5px solid white" allowfullscreen></iframe>
+						</div>
+					</div>   
+			        <?php
+			    endwhile;
+			endif;
+			?>
 		</div>
 	</div>			
 </section>
